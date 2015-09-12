21984e51fadae28d01459092a7fcf89fced891c1
Sandro Groganz <sandro@campaignchain.com>
2015-09-12 21:54:05 +0200

    CE-363 Assign copyright to CampaignChain Inc.

--- web/campaignchain/install.php
+++ web/campaignchain/install.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the CampaignChain package.
  *
- * (c) Sandro Groganz <sandro@campaignchain.com>
+ * (c) CampaignChain Inc. <info@campaignchain.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
