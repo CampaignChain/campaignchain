6f09bff8b6b33a3f6be2e7cfa6b0f2967070e3ca
Sandro Groganz <sandro@campaignchain.com>
2016-06-29 20:17:10 -0700

    CampaignChain/campaignchain#170 Add configuration files to run with Platform.sh

---
+++ app/config/parameters_platformsh.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * This file is part of the CampaignChain package.
+ *
+ * (c) CampaignChain, Inc. <info@campaignchain.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*
+ * This sets the database parameters for the Platform.sh environment. Find more
+ * information at: http://symfony.com/doc/2.8/cookbook/deployment/platformsh.html
+ */
+
+$relationships = getenv("PLATFORM_RELATIONSHIPS");
+if (!$relationships) {
+    return;
+}
+
+$relationships = json_decode(base64_decode($relationships), true);
+
+foreach ($relationships['database'] as $endpoint) {
+    if (empty($endpoint['query']['is_master'])) {
+        continue;
+    }
+
+    $container->setParameter('database_driver', 'pdo_' . $endpoint['scheme']);
+    $container->setParameter('database_host', $endpoint['host']);
+    $container->setParameter('database_port', $endpoint['port']);
+    $container->setParameter('database_name', $endpoint['path']);
+    $container->setParameter('database_user', $endpoint['username']);
+    $container->setParameter('database_password', $endpoint['password']);
+    $container->setParameter('database_path', '');
+}
+
+# Store session into /tmp.
+ini_set('session.save_path', '/tmp/sessions');
