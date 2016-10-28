629fd61b92134da67d754242e561e398ac5559c0
Sandro Groganz <sandro@campaignchain.com>
2016-10-28 23:25:32 +0200

    CampaignChain/campaignchain#261 Disable depreciation warnings in prod environment

--- web/app.php
+++ web/app.php
@@ -4,8 +4,6 @@ use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-Debug::enable(E_RECOVERABLE_ERROR & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED, false);
-
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
 // Use APC for autoloading to improve performance.
@@ -28,5 +26,6 @@ $kernel->loadClassCache();
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
+Debug::enable(E_RECOVERABLE_ERROR & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED, false);
 $response->send();
 $kernel->terminate($request, $response);
