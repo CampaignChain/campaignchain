66a841b270d68b61d383dad0cb723fa9e65cd5b5
Sandro Groganz <sandro@campaignchain.com>
2017-07-05 09:22:34 +0200

    CampaignChain/campaignchain#407 Implement Event Stream Processing (ESP) module

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -62,6 +62,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             // CampaignChain
             $bundles[] = new CampaignChain\GeneratorBundle\CampaignChainGeneratorBundle();
+            $bundles[] = new CampaignChain\Core\ESPBundle\CampaignChainCoreESPBundle();
         }
 
         return $bundles;
